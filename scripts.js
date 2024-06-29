9549c294ba82f986169cb7727194d3adb1dddb54
ABDULRAZAQ79922 <111866517+ABDULRAZAQ79922@users.noreply.github.com>
2024-06-29 23:46:49 +0530

    completed

---
+++ scripts.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const card = document.getElementById('business-card');
+    const profilePic = document.getElementById('profile-pic');
+    const name = document.getElementById('name');
+    const title = document.getElementById('title');
+    const contact = document.getElementById('contact');
+    const phone = document.getElementById('phone');
+    const linkedin = document.getElementById('linkedin');
+    const github = document.getElementById('github');
+    const twitter = document.getElementById('twitter');
+
+    
+    linkedin.href = "#";
+    github.href = "#";
+    twitter.href = "#";
+});
